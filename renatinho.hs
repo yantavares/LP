fa800f161af32e82e903e4840668311423b3a2f3
yantavares <yantdo1@gmail.com>
2022-12-21 18:42:43 -0300

    arquivo de testes renatinho

--- renatinho.hs
+++ renatinho.hs
@@ -4,3 +4,8 @@ eval (Lit n) = n
 eval (Add e1 e2) = (eval e1) + (eval e2)
 eval (Sub e1 e2) = (eval e1) - (eval e2)
 eval (Mult e1 e2) = eval e1 * eval e2
+
+last1 [x] = x
+last1 (x:xs) = last1 xs
+
+elem1 n l = l!!(n-1)
